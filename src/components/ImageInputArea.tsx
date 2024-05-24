274e2d7c4bd9ac47d28f36081075b3c49259f8f6
Daniel12346 <danezoki2@gmail.com>
2024-05-24 02:56:52 +0200

    added face bounding box, replaced .env with .env.local

--- src/components/ImageInputArea.tsx
+++ src/components/ImageInputArea.tsx
@@ -1,13 +1,23 @@
 // import { useState } from "react";
 
 import { DeleteIcon, PlusSquareIcon } from "@chakra-ui/icons";
-import { Center, Input, FormLabel, Text, Box, Spinner } from "@chakra-ui/react";
+import {
+  Center,
+  Input,
+  FormLabel,
+  Text,
+  Box,
+  Spinner,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
 const HiddenImageInputWithIcon = () => {
   const [imageSrc, setImageSrc] = useState<string | null>(null);
   const [isRecognizingFace, setIsRecognizingFace] = useState<boolean>(false);
   const [recognizedFaces, setRecognizedFaces] = useState<string[] | null>(null);
+  const [canvasUrl, setCanvasUrl] = useState<string | null>(null);
 
   const onImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -29,19 +39,29 @@ const HiddenImageInputWithIcon = () => {
         setIsRecognizingFace(true);
         const formData = new FormData();
         formData.append("image", image);
+        console.log(import.meta.env.VITE_FACE_RECOGNITION_URI);
         const response = await fetch(
-          `${import.meta.env.FACE_RECOGNITION_URL}/check-face`,
+          `${import.meta.env.VITE_FACE_RECOGNITION_URI}`,
           {
             method: "POST",
             body: formData,
           }
         );
         const data = await response.json();
-        if (!data?.result.length) {
+        console.log(data);
+        const { matchResults, detection, canvasUrl } = data;
+        if (matchResults?.length) {
+          setRecognizedFaces(matchResults.map((e: any) => e._label));
+        } else {
           console.log("No faces recognized");
           setRecognizedFaces(null);
-        } else {
-          setRecognizedFaces(data?.result.map((e: any) => e._label));
+        }
+
+        if (detection) {
+          //draw the bounding box
+        }
+        if (canvasUrl) {
+          setCanvasUrl(canvasUrl);
         }
 
         console.log(data);
@@ -54,51 +74,75 @@ const HiddenImageInputWithIcon = () => {
   };
 
   return (
-    <Center>
-      <Input
-        type="file"
-        accept="image/*"
-        onChange={onImageChange}
-        display="none"
-        id="image"
-        name="image"
-      />
+    <Stack>
+      <Center>
+        <Input
+          type="file"
+          accept="image/*"
+          onChange={onImageChange}
+          display="none"
+          id="image"
+          name="image"
+          position={"absolute"}
+        />
+        {/* <canvas id="canvas" position={}></canvas> */}
 
-      {imageSrc ? (
-        <Box>
-          <img src={imageSrc} alt="preview" />
-          <DeleteIcon
-            onClick={() => setImageSrc(null)}
-            cursor="pointer"
-            boxSize={6}
-            color="red.500"
-          ></DeleteIcon>
-        </Box>
-      ) : (
-        <FormLabel
-          htmlFor="image"
-          cursor="pointer"
-          boxSize="2xs"
-          placeContent="center"
-        >
-          <Center>
-            <PlusSquareIcon boxSize={12} color="blue.500" />
-            <Text>add an image</Text>
-          </Center>
-        </FormLabel>
-      )}
-      {isRecognizingFace && <Spinner />}
-      {recognizedFaces?.length && (
-        <>
-          <Text>Recognized faces:</Text>
-          <Box>
-            {recognizedFaces.map((name) => (
-              <Text key={name}>{name}</Text>
-            ))}
+        {imageSrc ? (
+          <Box position={"relative"}>
+            <img src={imageSrc} alt="preview" />
+            <img
+              hidden={!canvasUrl}
+              src={canvasUrl || ""}
+              id="overlay"
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                zIndex: 1,
+              }}
+            />
           </Box>
-        </>
-      )}
-    </Center>
+        ) : (
+          <FormLabel
+            htmlFor="image"
+            cursor="pointer"
+            boxSize="2xs"
+            placeContent="center"
+          >
+            <Center>
+              <PlusSquareIcon boxSize={12} color="blue.500" />
+              <Text>add an image</Text>
+            </Center>
+          </FormLabel>
+        )}
+      </Center>
+      <Box p={2}>
+        {isRecognizingFace && <Spinner />}
+        {recognizedFaces?.length && (
+          <Flex gap={5}>
+            <DeleteIcon
+              onClick={() => {
+                setImageSrc(null);
+                setRecognizedFaces(null);
+              }}
+              cursor="pointer"
+              boxSize={6}
+              color="red.500"
+            ></DeleteIcon>
+            <Flex>
+              <Text>Recognized faces:</Text>
+              <Box>
+                {recognizedFaces.map((name) => (
+                  <Text key={name}>{name}</Text>
+                ))}
+              </Box>
+            </Flex>
+          </Flex>
+        )}
+      </Box>
+    </Stack>
   );
 };
 
