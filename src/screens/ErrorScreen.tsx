523f6b212542da2b17ecacbc4da6e70ce61e4aa9
Daniel12346 <danezoki2@gmail.com>
2024-09-03 02:51:52 +0200

    improved error screen

--- src/screens/ErrorScreen.tsx
+++ src/screens/ErrorScreen.tsx
@@ -1,14 +1,31 @@
-import { Center, Stack, Text } from "@chakra-ui/react";
-import { useRouteError } from "react-router-dom";
+import { Button, Center, Container, Stack, Text } from "@chakra-ui/react";
+import { Link as ReactRouterLink, useRouteError } from "react-router-dom";
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 export default function ErrorScreen() {
   const error = useRouteError();
   console.log(error);
   return (
-    <Stack gap={4} width={{ base: "100%" }} maxWidth={"25rem"} px="4">
-      <Center>
-        <Text>Error </Text>
+    <Container>
+      <Center paddingTop={10} px={{ base: "10px" }}>
+        <Stack gap={6}>
+          <Text fontSize={"4xl"} fontWeight={"semibold"}>
+            Oops! Something went wrong
+          </Text>
+          <Center>
+            <ChakraLink as={ReactRouterLink}>
+              <Button
+                size={"lg"}
+                colorScheme={"blue"}
+                rightIcon={<ArrowForwardIcon />}
+              >
+                Return home
+              </Button>
+            </ChakraLink>
+          </Center>
+        </Stack>
       </Center>
-    </Stack>
+    </Container>
   );
 }
